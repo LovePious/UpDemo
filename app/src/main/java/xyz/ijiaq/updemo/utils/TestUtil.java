bc2764cc6692a2f8e52bc12477afae5578519da8
Pious <only@ijiaqi.xyz>
2018-09-04 17:20:08 +0800

    测试文本

---
+++ app/src/main/java/xyz/ijiaq/updemo/utils/TestUtil.java
@@ -0,0 +1,11 @@
+package xyz.ijiaq.updemo.utils;
+
+import android.content.Context;
+
+import xyz.ijiaq.updemo.MainActivity;
+
+public class TestUtil {
+    public static void start(Context mContext) {
+        MainActivity.start(mContext);
+    }
+}
